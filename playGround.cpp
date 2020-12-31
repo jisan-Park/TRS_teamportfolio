cfcafb4f306bfab5723d9a4519db4eb2c3ee3e93
jisan-Park <jsnpark1221@gmail.com>
2020-12-31 16:49:52 +0900

    2020-12-31 렉트 z-order
    렉트 별 y축 기준 render용

---
+++ playGround.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "playGround.h"
+
+playGround::playGround()
+{
+}
+
+
+playGround::~playGround()
+{
+}
+
+//초기화는 여기다 하세요
+HRESULT playGround::init()
+{
+	gameNode::init(true);
+
+	IMAGEMANAGER->addImage("배경", "background.bmp", WINSIZEX, WINSIZEY, true, RGB(255, 0, 255));
+
+	return S_OK;
+}
+
+//메모리 해제는 여기다 하세요
+void playGround::release()
+{
+	gameNode::release();
+
+}
+
+//연산(계산)은 여기다 하세요
+void playGround::update()
+{
+	gameNode::update();
+	
+}
+
+//제발 여기다 그려라 좀...
+void playGround::render()
+{
+	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
+	//================ 위에 건들지 마라 ==============================
+	
+	IMAGEMANAGER->findImage("배경")->render(getMemDC());
+
+	//================= 아래도 건들지 마라 ==============================
+	_backBuffer->render(getHDC());
+}
+
