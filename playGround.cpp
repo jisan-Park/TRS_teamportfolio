aa0014784d43ebd73230e5a09ac916fa07445bf1
namong2 <nmnm_e@naver.com>
2020-12-31 17:43:59 +0900

    Revert "나몽부농렉트생성"
    This reverts commit 1b42f21a9f98253178997f799e558e2f2ede56f2.

--- playGround.cpp
+++ playGround.cpp
@@ -41,17 +41,9 @@ void playGround::render()
 	//================ 위에 건들지 마라 ==============================
 	RECT rc = RectMakeCenter(WINSIZEX/2, WINSIZEY/2,100,100);
 	RECT jhrc = RectMakeCenter(WINSIZEX / 2, WINSIZEY / 2 - 300, 100, 100);
-	RECT nmrc = RectMakeCenter(WINSIZEX / 2, WINSIZEY /2 + 200, 100, 100);
 	IMAGEMANAGER->findImage("배경")->render(getMemDC());
 	Rectangle(getMemDC(),rc);
 	Rectangle(getMemDC(), jhrc);
-
-	HBRUSH brush = CreateSolidBrush(RGB(244, 170, 197));
-	HBRUSH oldBrush = (HBRUSH)SelectObject(getMemDC(), brush);
-	Rectangle(getMemDC(), nmrc);
-	SelectObject(getMemDC(), oldBrush);
-	DeleteObject(brush);
-
 	//================= 아래도 건들지 마라 ==============================
 	_backBuffer->render(getHDC());
 }
