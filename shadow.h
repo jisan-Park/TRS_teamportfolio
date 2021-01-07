40454cc4ad4abb15f61eea923013a84199c2b7e7
DESKTOP-S83D6OK\조주연 <ssgwon1@gmail.com>
2021-01-07 16:11:23 +0900

    2021-01-07
    (진혁)
    shadow.h

--- shadow.h
+++ shadow.h
@@ -6,7 +6,7 @@ struct shadow
 
 	float z; // 그림자 z값
 	float distance; // 그림자와 플레이어 사이 거리(플레이어 z값)
-	
+
 	float hPushPower; // 가로 미는 힘(음수면 왼쪽으로 움직이고 양수면 오른쪽으로 움직임)
 	float vPushPower; // 세로 미는 힘(음수면 위쪽으로 움직이고 양수면 아래쪽으로 움직임)
 };
