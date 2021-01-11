705371ce17b1b97fd28f0ea52dc46baf7e9880f3
Jin Hyuck Jang <mrsnaky97@gmail.com>
2021-01-11 12:18:52 +0900

    2020-01-11 (진혁) 푸시파워 바꿔주던거 pt좌표 바꿔주는걸로 바꿨음

--- botLine.cpp
+++ botLine.cpp
@@ -5,16 +5,17 @@ void botLine::collision(characterInfo &info)
 {
 	if (intersects(info.ptrc))
 	{
-		if (info.vPushPower < 0 && info.ptDistance >= passRange) // 점프해서 넘어가려고 할때 허용치보다 높이 뛰면 그림자 높이 올려주기
+		if (info.vPushPower <= 0 && info.ptDistance > passRange) // 점프해서 넘어가려고 할때 허용치보다 높이 뛰면 그림자 높이 올려주기
 		{
 			info.shd_height = passRange;
 		}
-		else if (info.vPushPower < 0 && info.ptDistance <= passRange) // 넘어가려고 할 때 허용치보다 낮게 뛰고 있거나 땅에 있으면 못가게 막기
+		else if (info.vPushPower < 0 && info.ptDistance < passRange) // 넘어가려고 할 때 허용치보다 낮게 뛰고 있거나 땅에 있으면 못가게 막기
 		{
-			info.vPushPower = 0;
-			info.pt_y += 2.5f;
+			/*		info.vPushPower = 0;
+					info.pt_y += 2.5f;*/
+			info.pt_y -= info.vPushPower;
 		}
-		if (info.vPushPower > 0 && info.shd_height == passRange) // 그림자 높이가 해당 선 허용치랑 같고 떨어지려고 할 때 
+		if (info.vPushPower > 0 && info.shd_height >= passRange) // 그림자 높이가 해당 선 허용치랑 같고 떨어지려고 할 때 
 		{
 			info.shd_height -= passRange;
 		}
--- rightLine.cpp
+++ rightLine.cpp
@@ -5,14 +5,14 @@ void rightLine::collision(characterInfo &info)
 {
 	if (intersects(info.ptrc))
 	{
-		if (info.hPushPower < 0 && info.ptDistance >= passRange) // 왼쪽으로 가려고 할때 허용치보다 높이 뛰면 그림자 높이 올려주기
+		if (info.hPushPower < 0 && info.ptDistance > passRange) // 왼쪽으로 가려고 할때 허용치보다 높이 뛰면 그림자 높이 올려주기
 		{
 			info.shd_height = passRange;
 		}
-		else if (info.hPushPower < 0 && info.ptDistance <= passRange) // 왼쪽으로 가려고 할 때 허용치보다 낮게 뛰고 있거나 땅에 있으면 못가게 막기
+		else if (info.hPushPower < 0 && info.ptDistance < passRange) // 왼쪽으로 가려고 할 때 허용치보다 낮게 뛰고 있거나 땅에 있으면 못가게 막기
 		{
-			info.hPushPower = 0;
-			info.pt_x += 2.5f;
+			//info.hPushPower = 0;
+			info.pt_x += info.hPushPower;
 		}
 
 		if (info.hPushPower > 0 && info.shd_height == passRange) // 높이 있을때 오른쪽으로 가면 떨어지게 하기
@@ -20,14 +20,14 @@ void rightLine::collision(characterInfo &info)
 			info.shd_height -= passRange;
 		}
 
-		if (info.vPushPower > 0 && info.ptDistance >= passRange) // 점프해서 넘어가려고 할때 허용치보다 높이 뛰면 그림자 높이 올려주기
+		if (info.vPushPower > 0 && info.ptDistance > passRange) // 점프해서 넘어가려고 할때 허용치보다 높이 뛰면 그림자 높이 올려주기
 		{
 			info.shd_height = passRange;
 		}
-		else if (info.vPushPower > 0 && info.ptDistance <= passRange) // 넘어가려고 할 때 허용치보다 낮게 뛰고 있거나 땅에 있으면 못가게 막기
+		else if (info.vPushPower > 0 && info.ptDistance < passRange) // 넘어가려고 할 때 허용치보다 낮게 뛰고 있거나 땅에 있으면 못가게 막기
 		{
-			info.vPushPower = 0;
-			info.pt_y -= 2.5f;
+			//info.vPushPower = 0;
+			info.pt_y -= info.vPushPower;
 		}
 		if (info.vPushPower < 0 && info.shd_height == passRange) // 그림자 높이가 해당 선 허용치랑 같고 떨어지려고 할 때 
 		{
